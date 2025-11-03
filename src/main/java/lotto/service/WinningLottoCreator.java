8f972ff8d7964e5cfe6a256963483b0d972e22a2
yeahh-jj <jyll1009@gmail.com>
2025-11-03 15:02:36 +0900

    feat: create WinningLottoCreator

--- src/main/java/lotto/service/WinningLottoCreator.java
+++ src/main/java/lotto/service/WinningLottoCreator.java
@@ -1,2 +1,19 @@
-package lotto.service;public class WinningLottoCreator {
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class WinningLottoCreator {
+
+    public List<Integer> createWinningNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    public int createBonusNumber(List<Integer> winningNumbers) {
+        int bonusNum;
+        do {
+            bonusNum = Randoms.pickNumberInRange(1, 45);
+        } while (winningNumbers.contains(bonusNum));
+        return bonusNum;
+    }
 }
