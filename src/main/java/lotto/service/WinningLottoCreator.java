e0cfde63be78e2b894b6db1c8e5842f443210540
yeahh-jj <jyll1009@gmail.com>
2025-11-03 16:27:13 +0900

    feat: update WinningLottoCreator and create WinningLottoCreatorTest

--- src/main/java/lotto/service/WinningLottoCreator.java
+++ src/main/java/lotto/service/WinningLottoCreator.java
@@ -4,12 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class WinningLottoCreator {
-
-    public List<Integer> createWinningNumbers() {
+    public static List<Integer> createWinningNumbers() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 
-    public int createBonusNumber(List<Integer> winningNumbers) {
+    public static int createBonusNumber(List<Integer> winningNumbers) {
         int bonusNum;
         do {
             bonusNum = Randoms.pickNumberInRange(1, 45);
--- src/test/java/lotto/service/WinningLottoCreatorTest.java
+++ src/test/java/lotto/service/WinningLottoCreatorTest.java
@@ -1,2 +1,28 @@
-package lotto.service;public class WinningLottoCreatorTest {
+package lotto.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class WinningLottoCreatorTest {
+    @Test
+    void 로또_번호가_6개_중복없이_1에서_45_사이의_숫자로_생성되는지_확인() {
+        List<Integer> numbers = WinningLottoCreator.createWinningNumbers();
+
+        assertThat(numbers).hasSize(6);
+        assertThat(new HashSet<>(numbers)).hasSize(6);
+        assertThat(numbers).allMatch(n -> n >= 1 && n <= 45);
+    }
+
+
+    @Test
+    void 보너스_번호가_당첨_번호와_중복되지_않고_1에서_45_사이의_숫자인지_확인() {
+        List<Integer> numbers = WinningLottoCreator.createWinningNumbers();
+        int bonus = WinningLottoCreator.createBonusNumber(numbers);
+
+        assertThat(bonus).isBetween(1, 45);
+        assertThat(numbers).doesNotContain(bonus);
+    }
 }
