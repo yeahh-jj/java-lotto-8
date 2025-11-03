d2e4ece7c69fb45ef0b1e41fe1f1ab32f95fa57d
yeahh-jj <jyll1009@gmail.com>
2025-11-03 19:41:16 +0900

    feat: create ServiceTest and Service

--- src/main/java/lotto/service/Service.java
+++ src/main/java/lotto/service/Service.java
@@ -1,4 +1,47 @@
 package lotto.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+import lotto.domain.Lotto;
+import lotto.domain.Rank;
+import lotto.domain.WinningNumbers;
+import lotto.util.Validator;
+
 public class Service {
+    private static final int PRICE = 1000;
+
+    public List<Lotto> purchaseLottos(int purchaseAmount) {
+        Validator.validatePurchaseAmount(purchaseAmount);
+        int count = purchaseAmount / PRICE;
+
+        return IntStream.range(0, count)
+                .mapToObj(i -> new Lotto(LottoNumberGenerator.createWinningNumbers()))
+                .toList();
+    }
+
+    public Map<Rank, Long> calculateRanks(List<Lotto> lottos, WinningNumbers winningNumbers) {
+        Map<Rank, Long> rankResult = new HashMap<>();
+
+        for (Lotto lotto : lottos) {
+            Rank rank = winningNumbers.rankOf(lotto);
+            rankResult.put(rank, rankResult.getOrDefault(rank, 0L) + 1);
+        }
+
+        return rankResult;
+    }
+
+    public double calculateProfitRate(Map<Rank, Long> rankResults, int purchaseAmount) {
+        long totalPrize = 0;
+
+        for (Map.Entry<Rank, Long> entry : rankResults.entrySet()) {
+            Rank rank = entry.getKey();       // 등수
+            long count = entry.getValue();    // 해당 등수 개수
+            totalPrize += rank.getPrizeMoney() * count;  // 등수별 상금 * 개수
+        }
+
+        double profitRate = (double) totalPrize / purchaseAmount * 100;
+        return Math.round(profitRate * 10) / 10.0; // 소수점 둘째 자리 반올림
+    }
 }
---
+++ src/test/java/lotto/service/ServiceTest.java
@@ -0,0 +1,56 @@
+package lotto.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lotto.domain.Lotto;
+import lotto.domain.Rank;
+import lotto.domain.WinningNumbers;
+import org.junit.jupiter.api.Test;
+
+public class ServiceTest {
+    @Test
+    void 구입_금액에_따라_로또_개수가_발행되는지_확인() {
+        Service service = new Service();
+
+        List<Lotto> lottos = service.purchaseLottos(8000);
+
+        assertThat(lottos).hasSize(8);
+    }
+
+    @Test
+    void 당첨_번호와_비교해서_등수가_맞게_집계되는지_확인() {
+        Service service = new Service();
+        WinningNumbers winningNumbers = new WinningNumbers(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7
+        );
+
+        List<Lotto> lottos = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 8)),
+                new Lotto(List.of(1, 2, 3, 4, 8, 9)),
+                new Lotto(List.of(11, 12, 13, 15, 16, 17))
+        );
+
+        Map<Rank, Long> result = service.calculateRanks(lottos, winningNumbers);
+
+        assertThat(result.get(Rank.FIRST)).isEqualTo(1);
+        assertThat(result.get(Rank.THIRD)).isEqualTo(1);
+        assertThat(result.get(Rank.FOURTH)).isEqualTo(1);
+        assertThat(result.get(Rank.NONE)).isEqualTo(1);
+    }
+
+    @Test
+    void 수익률이_제대로_계산되는지_검증() {
+        Service service = new Service();
+        Map<Rank, Long> rankResults = new HashMap<>();
+        rankResults.put(Rank.FIFTH, 1L);
+        int purchaseAmount = 8000;
+
+        double profitRate = service.calculateProfitRate(rankResults, purchaseAmount);
+
+        assertThat(profitRate).isEqualTo(62.5);
+    }
+}
